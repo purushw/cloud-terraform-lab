2d98baade5eb1ba91fea1d317b01bd146ebcaffa
Purush <pwizzy999@gmail.com>
2026-08-23 14:36:33 +0100

    Migrate VPC lab to S3 remote state

--- aws/01-vpc/versions.tf
+++ aws/01-vpc/versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = "~> 1.13.0"
 
+  backend "s3" {
+    bucket       = "purush-cloud-terraform-state"
+    key          = "aws/01-vpc/terraform.tfstate"
+    region       = "eu-west-2"
+    profile      = "purush-lab"
+    use_lockfile = true
+  }  
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,3 +16,4 @@ terraform {
     }
   }
 }
+
